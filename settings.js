25db9d9515e8356841ee64c7316fed6d08bce9f6
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-05-21 17:11:57 +0545

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,17 +7,17 @@ module.exports = {
       prefix: '?',
       token: '',
       invLink: 'https://discord.com/api/oauth2/authorize?client_id=${}&permissions=8&scope=bot%20applications.commands',
-      privacy: 'https://discord.gg/codersplanet',
-      terms: 'https://discord.gg/codersplanet',
+      privacy: 'https://discord.gg/GPzYFx7zfe',
+      terms: 'https://discord.gg/GPzYFx7zfe',
     },
     presence: {
       name: status,
       type: 'LISTENING',
-      url: 'https://twitch.tv/axo'
+      url: 'https://twitch.tv/supreme'
     },
     credits: {
-      developerId: '1219309281011171390',
-      supportServer: 'https://discord.gg/codersplanet'
+      developerId: '1131953433134497923',
+      supportServer: 'https://discord.gg/GPzYFx7zfe'
     },
   }
 }
